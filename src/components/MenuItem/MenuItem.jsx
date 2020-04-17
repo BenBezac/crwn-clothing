a2b01d6114d13a8a89484933c250ff586ac4d2bb
BenBezac <benjamin.bezancon@gmail.com>
2020-04-17 11:32:34 +0200

    Split homepage into components

---
+++ src/components/MenuItem/MenuItem.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './MenuItem.scss';
+import {useTranslation} from "react-i18next";
+
+const MenuItem = ({title, imageUrl, size}) => {
+    const { t, i18n } = useTranslation();
+
+    return (
+        <div
+            className={`${size} menu-item`}
+        >
+            <div
+                className="background-image"
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}/>
+            <div className="content">
+                <h1 className="title">{t(`menu-item-types.${title}`).toUpperCase()}</h1>
+                <span className="subtitle">{t('shop-now').toUpperCase()}</span>
+            </div>
+        </div>
+    );
+};
+
+export default MenuItem;
