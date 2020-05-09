773bc60991ff4903cd8062db93b0dc12364b7977
BenBezac <benjamin.bezancon@gmail.com>
2020-05-09 15:36:52 +0200

    Connect CartDropdown to reducer

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
 
-import './cart-dropdown.styles.scss'
-import CustomButton from "../custom-button/custom.button";
-import {useTranslation} from "react-i18next";
+import './cart-dropdown.styles.scss';
+import CustomButton from '../custom-button/custom.button';
+import { useTranslation } from 'react-i18next';
+import { connect } from 'react-redux';
+import CartItem from "../cart-item/cart-item.component";
 
-const CartDropdown = () => {
-    const {t} = useTranslation();
-    return <div className="cart-dropdown">
-        <div className="cart-items"></div>
-        <CustomButton>{t('cart.go-to-checkout').toUpperCase()}</CustomButton>
-    </div>;
+const CartDropdown = ({ cartItems }) => {
+    const { t } = useTranslation();
+    return (
+        <div className="cart-dropdown">
+            <div className="cart-items">
+                {
+                    cartItems.map(
+                        cartItem => <CartItem key={cartItem.id} item={cartItem}/>
+                    )
+                }
+            </div>
+            <CustomButton>
+                {t('cart.go-to-checkout').toUpperCase()}
+            </CustomButton>
+        </div>
+    );
 };
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    cartItems,
+});
+
+export default connect(mapStateToProps)(CartDropdown);
