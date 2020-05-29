08de3a2c311027c7d89dcf75b7e510474acf21d6
Ben <benjamin.bezancon@gmail.com>
2020-05-29 18:47:08 +0200

    Refactor root reducer and user redux functionality with typescript - Fix in user actions

--- src/redux/user/user.actions.ts
+++ src/redux/user/user.actions.ts
@@ -1,7 +1,8 @@
 import { User, SET_CURRENT_USER, UserActionTypes} from "./types";
 
-
-export const setCurrentUser = (user: User): UserActionTypes => ({
-    type: SET_CURRENT_USER,
-    payload: user
-})
+export const setCurrentUser = (user: User): UserActionTypes => {
+    return {
+        type: SET_CURRENT_USER,
+        payload: user
+    }
+}
